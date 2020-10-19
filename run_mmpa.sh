99b7bff67ff49e47d15d5e2f8022ed4c64cf7635
Thomas Blaschke <tblaschke@users.noreply.github.com>
2020-10-19 16:28:46 +0200

    add additional analysis

--- run_mmpa.sh
+++ run_mmpa.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 set -e 
 
-jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --inplace --execute extract_scaffold_memory.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --inplace --execute extract_scaffold_memory.part1.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --inplace --execute extract_scaffold_memory.part2.ipynb
 
 python mmpa_fragmentation.py
 parallel < to_fragment/jobs 
@@ -9,4 +10,6 @@ parallel < to_fragment/jobs
 python mmpa_indexing.py
 parallel < fragmented_smiles/jobs
 
-jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --inplace --execute analyze_mmpa.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --inplace --execute analyze_mmpa.part1.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --inplace --execute analyze_mmpa.part2.ipynb
+
